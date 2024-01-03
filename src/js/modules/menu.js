e68d0472855c6e80feac894fc2e003dddf6ba8d4
Higor Costa <higorcosta972@gmail.com>
2024-01-03 20:00:11 -0300

    feat: Verifica a presença da classe "ativo" no svg
    O item "Grupos" do menu possui um icone ao seu lado, que é rotacionado de acordo com a presença da classe

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -4,7 +4,10 @@ const tituloListaGrupos = document.querySelector('#titulo-grupos');
 
 const controlaExibicaoGrupos = () => {
   const grupos = document.querySelector('#lista-grupos');
+  const setaGrupos = document.querySelector('#seta-grupos');
+
   grupos.classList.toggle('ativo');
+  setaGrupos.classList.toggle('ativo');
 }
 
 tituloListaGrupos.addEventListener('click', controlaExibicaoGrupos);
