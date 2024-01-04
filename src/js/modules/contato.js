2bb3ef4a778fae3dee0abb61a60fa4f23e746048
Higor Costa <higorcosta972@gmail.com>
2024-01-04 20:09:01 -0300

    feat: Adiciona interavidade ao icone de opções de cada contato.
    Uma função para controlar a exibição da caixa de opcões de cada contato.

--- src/js/modules/contato.js
+++ src/js/modules/contato.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const botoesOpcoesContato = document.querySelectorAll('.opcoes-contato');
+
+const exibeOpcoesContato = ({ currentTarget }) => {
+  const iconeOpcoes = currentTarget;
+  const contato = iconeOpcoes.parentNode;
+  const opcoes = document.querySelector('#botoes-opcoes')
+
+  contato.appendChild(opcoes);
+  opcoes.classList.toggle('ativo');
+}
+
+botoesOpcoesContato.forEach(botao => {
+  botao.addEventListener('click', exibeOpcoesContato);
+});
