d2926117e602790d0aec5dbf628c4a43999852e6
higor-costa <higorcosta972@gmail.com>
2024-01-17 20:34:05 -0300

    Chore: Importa modal.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,4 @@ import menu from './modules/menu.js';
 import contato from './modules/contato.js';
 import paginacao from './modules/paginacao.js';
 import crud from './modules/crud.js';
+import modal from './modules/modal.js';
