ad4947466e69a55cbb4bab8e55e8800d2e147166
higor-costa <higorcosta972@gmail.com>
2024-01-17 20:40:16 -0300

    Feat: Adiciona objeto para controlar o modal
    O objeto contém métodos que definem quando o modal e seus elementos serão abertos ou fechados

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -1,3 +1,20 @@
 'use strict';
 
 import html from './paginacao.js';
+
+const elementos = {
+  modais: html.get('.modais'),
+  formulario: html.get('form'),
+  botaoAdicionarContato: html.get('.adicionar-contato'),
+  botaoCancelar: html.get('.cancelar'),
+  botaoAdicionar: html.get('.adicionar'),
+};
+
+const controlaModal = {
+  modal(acao) {
+    elementos.modais.classList.toggle('ativo', acao === 'Adicionar contato');
+  },
+  formularioContato(acao) {
+    elementos.formulario.classList.toggle('ativo', acao === 'Adicionar contato');
+  },
+};
