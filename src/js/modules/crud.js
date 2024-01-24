215c94a40ca236edf8d937dc83c8125feec466d7
higor-costa <higorcosta972@gmail.com>
2024-01-23 21:01:50 -0300

    Chore: Declara objeto para receber campos do form
    O objeto "camposFormulario" contém propriedades referentes a cada campo do formulário

--- src/js/modules/crud.js
+++ src/js/modules/crud.js
@@ -7,3 +7,10 @@ const acoes = {
   adicionar: document.querySelector('.adicionar'),
 }
 const dados = [];
+const camposFormulario = {
+  nome: document.querySelector('#nome'),
+  numero: document.querySelector('#numero'),
+  email: document.querySelector('#email'),
+  endereco: document.querySelector('#endereco'),
+  grupo: document.querySelector('#grupo'),
+}
