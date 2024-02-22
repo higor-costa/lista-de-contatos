44a1c97042753db3e825451d67984ae1c865e2e5
Higor Costa <higorcosta972@gmail.com>
2024-02-22 10:49:53 -0300

    Chore: Altera ordem de importação dos módules

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import menu from './modules/menu.js';
 import contato from './modules/contato.js';
+import modal from './modules/modal.js';
 import paginacao from './modules/paginacao.js';
 import crud from './modules/crud.js';
-import modal from './modules/modal.js';
