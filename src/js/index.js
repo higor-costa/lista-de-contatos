e62d828c1acc698e263e4e87f1fff57984e3b613
Higor Costa <higorcosta972@gmail.com>
2024-02-28 10:39:15 -0300

    Chore: Importa lixeira.js em index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,5 @@ import menu from './modules/menu.js';
 import contato from './modules/contato.js';
 import modal from './modules/modal.js';
 import paginacao from './modules/paginacao.js';
+import lixeira from './modules/lixeira.js';
 import crud from './modules/crud.js';
