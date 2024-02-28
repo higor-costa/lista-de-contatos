70e426efa4420563844fcdec42951cac9374556d
Higor Costa <higorcosta972@gmail.com>
2024-02-28 10:21:17 -0300

    Feat: Adiciona função para exibir apenas os contatos excluídos

---
+++ src/js/modules/lixeira.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import { contatosExcluidosModificado, eventoArrayModificado } from './crud.js';
+
+const menuLixeira = document.querySelector('#lixeira');
+
+let arrayAtualizado = [];
+contatosExcluidosModificado.on('excluidosModificados', (contatosExcluidos) => {
+  arrayAtualizado = contatosExcluidos;
+});
+
+function exibeContatosExcluidos() {
+  eventoArrayModificado.emit('arrayModificado', arrayAtualizado);
+}
+
+// Eventos
+menuLixeira.addEventListener('click', exibeContatosExcluidos);
