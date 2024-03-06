ea80a6b59ab348f7111c62e668d857b8b573fe50
Higor Costa <higorcosta972@gmail.com>
2024-03-06 20:08:12 -0300

    Feat: Declara o objeto "armazenamentoLixeira" para conter os métodos de enviar e puxar dados do localStorage

--- src/js/modules/lixeira.js
+++ src/js/modules/lixeira.js
@@ -5,6 +5,16 @@ import { contatosExcluidosModificado, eventoArrayModificado } from './crud.js';
 const menuLixeira = document.querySelector('#lixeira');
 let arrayAtualizado;
 
+const armazenamentoLixeira = {
+  enviaDados(contatosExcluidos) {
+    localStorage.setItem('Lixeira', JSON.stringify(contatosExcluidos));
+  },
+  puxaDados() {
+    const arrayContatosExcluidos = JSON.parse(localStorage.getItem('Lixeira'));
+    return arrayContatosExcluidos;
+  },
+}
+
 const inicializaLixeira = () => {
   arrayAtualizado = armazenamentoLixeira.puxaDados() ?? [];
 }
